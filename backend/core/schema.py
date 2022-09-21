5136914c3ae6a29db85cc82bc08eb6e27f79d52a
Filip Dimitrievski <filipdimitrievski@protonmail.com>
2022-09-20 21:00:52 -0400

    Basic graphene set up

---
+++ backend/core/schema.py
@@ -0,0 +1,13 @@
+from urllib import request
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hi!")
+
+
+# class Mutation(graphene.ObjectType):
+#     pass
+
+
+schema = graphene.Schema(query=Query) # add mutation=Mutation
---
+++ backend/core/types.py
@@ -0,0 +1,6 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+
+
+class DummyType(DjangoObjectType):
+    pass
